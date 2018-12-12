acb5639e75f228950b198293141254e63625d36a
liwy <lwy33445@sina.com>
2018-12-12 14:11:24 +0800

    音乐播放

---
+++ src/components/common/MusicPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="container">
+    <div id="music-player">
+      <audio controls="controls" autoplay="autoplay" loop="loop">
+        <source src="/static/audio/dream.mp3" type="audio/mpeg"/>
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MusicPlayer',
+    data () {
+      return {}
+    },
+    created () {
+    },
+    mounted () {
+      // let audio = document.getElementsByTagName('audio')
+      // if (this.isPlay && audio.paused) {
+      //   audio.play()
+      // }
+
+    },
+    computed: {
+      isPlay () {
+        return this.$store.getters.isPlay
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
